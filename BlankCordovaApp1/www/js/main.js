ac6579dd2f6c8e41ae9716371b21f54dee1eba31
Bruce LANE <bruce@batchass.fr>
2015-05-11 12:12:57 +0200

    v2 wip

--- BlankCordovaApp1/www/js/main.js
+++ BlankCordovaApp1/www/js/main.js
@@ -1,42 +1,38 @@
-document.addEventListener('DOMContentLoaded', function(){
-	
-	//map (leaflet : http://leafletjs.com/examples/quick-start.html)
-	var map = L.map('myMap');
-	map.setView([43.7139654, 7.2891412], 16);
+document.addEventListener('DOMContentLoaded', function () {
 
-	//map pattern (WTMS). mapbox, ign, osm, ... 
-	//var mapPatternUrl = "http://tile.stamen.com/toner/{z}/{x}/{y}.png";
-    var mapPatternUrl = "http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg";
-	
-	//var mapPatternUrl = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var tileLayer = L.tileLayer(mapPatternUrl);
-	tileLayer.addTo(map);
+    var status = document.querySelector("#status");
 
-	//marker
-	var marker = L.marker([0, 0]);
-	marker.bindPopup("The marker.");
-	marker.addTo(map);
+    //map 
+    var map = L.map('myMap');
+    map.setView([0, 0], 3);
 
-	//shape circle
-	var circle = L.circle([0, 0], 500000);
-	circle.bindPopup("The circle.");
-	circle.addTo(map);
+    //pattern
+    //map pattern (WTMS). mapbox, ign, osm, ... 
+    //var mapPatternUrl = "http://tile.stamen.com/toner/{z}/{x}/{y}.png";
+    //var mapPatternUrl = "http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg";
+    var mapPatternUrl = 'http://tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var tileLayer = L.tileLayer(mapPatternUrl);
+    tileLayer.addTo(map);
 
-	//shape polygon
-	var polygon = L.polygon([
-	    [-1, -1],
-	    [1, -1],
-	    [1, 1],
-	    [-1, 1],
-	]);
-	polygon.bindPopup("The polygon.");
-	polygon.addTo(map);
+    //marker
+    var marker = L.marker([0, 0]);
+    marker.bindPopup("Me.");
+    marker.addTo(map);
 
-	
-	function onMapClick(e) {
-		marker.setLatLng(e.latlng);
-	}
-	map.on('click', onMapClick);
-	
+    function processPosition(event) {
+        status.innerHTML = "Lat : " + event.coords.latitude + "° Long : " + event.coords.longitude + "° Precision : " + event.coords.accuracy + "m.";
+        marker.setLatLng([event.coords.latitude, event.coords.longitude]);
+
+    }
+
+    function errorPosition() {
+        status.innerHTML = "No position.";
+        marker.setLatLng([0, 0]);
+    }
+
+    //location notifications
+    var options = { "enableHighAccuracy": true, "maximumAge": 0, "timeout": Infinity };
+    navigator.geolocation.watchPosition(processPosition, errorPosition, options);
+
 
 });
