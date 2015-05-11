02033ec87745c3d88798da3d57d98708d080be54
Bruce LANE <bruce@batchass.fr>
2015-05-11 12:29:22 +0200

    2 wip

--- BlankCordovaApp1/www/js/main.js
+++ BlankCordovaApp1/www/js/main.js
@@ -19,10 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
     marker.bindPopup("Me.");
     marker.addTo(map);
 
+    //shape circle
+    var circle = L.circle([0, 0], 500000);
+    circle.bindPopup("The circle.");
+    circle.addTo(map);
+
     function processPosition(event) {
         status.innerHTML = "Lat : " + event.coords.latitude + "° Long : " + event.coords.longitude + "° Precision : " + event.coords.accuracy + "m.";
         marker.setLatLng([event.coords.latitude, event.coords.longitude]);
-
+        map.setView([event.coords.latitude, event.coords.longitude], 16);
+        circle.setLatLng([event.coords.latitude, event.coords.longitude]);
+        circle.setRadius(event.coords.accuracy);
     }
 
     function errorPosition() {
